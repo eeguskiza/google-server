76bc072a5390f6a89491f4413a5fdfff81ca9264
eeguskiza <e.eguskiza@gmail.com>
2024-12-01 17:56:07 +0100

    Initial Commit

---
+++ src/main/java/com/deusto/google_server/GoogleServerApplication.java
@@ -0,0 +1,13 @@
+package com.deusto.google_server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleServerApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GoogleServerApplication.class, args);
+    }
+
+}
